3d4b28bfd2c98f8815a66651cda1709c27b747c4
hayden <hemc7@sbcglobal.net>
2025-09-24 21:51:10 -0500

    added template code for basic window

---
+++ gui.py
@@ -0,0 +1,17 @@
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
