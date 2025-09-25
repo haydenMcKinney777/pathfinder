69bc675b4eac904e3665d29f65b5859889d637e4
hayden <hemc7@sbcglobal.net>
2025-09-24 22:18:43 -0500

    added cell class for rectangles

--- gui.py
+++ gui.py
@@ -1,17 +1,37 @@
 import sys
 import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6 import QtWidgets, QtGui
 
-class MyWidget(QtWidgets.QWidget):
+class Cell(QtWidgets.QGraphicsRectItem):
+    def __init__(self, x, y, size, row, col):
+        super().__init__(x, y, size, size)
+        self.row = row
+        self.col = col
+        self.setPen(QtGui.QPen(QtGui.QColor("black")))
+        self.setBrush(QtGui.QBrush(QtGui.QColor("grey")))
+
+
+class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        
+
+        self.scene = QtWidgets.QGraphicsScene()
+        self.view = QtWidgets.QGraphicsView(self.scene)
+
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.view)
+
+        cell_size = 50
+        for row in range(20):
+            for col in range(20):
+                cell = Cell(col * cell_size, row * cell_size, cell_size, row, col)
+                self.scene.addItem(cell)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
+    window = Window()
+    window.resize(1200, 1200)
+    window.show()
 
     sys.exit(app.exec())
