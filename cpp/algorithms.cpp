f006363eefbe2ddf87b202aef01351f77beb9cc8
hayden <hemc7@sbcglobal.net>
2025-10-22 20:15:57 -0500

    fixed cout mistake in algorithms.cpp

--- cpp/algorithms.cpp
+++ cpp/algorithms.cpp
@@ -4,28 +4,28 @@
 
 int dijkstra_run(const std::vector<std::vector<int>>&grid, int start_row, int start_col, int goal_row, int goal_col) {
     std::cout << "Dijkstra run called\n";
-    std::cout << "start row:" << " start_row" << "\nstart col:" << " start_col" << std::endl;
-    std::cout << "goal row:" << " goal_row" << "\ngoal col:" << " goal_col" << std::endl;
+    std::cout << "start row: " << start_row << "\nstart col: " << start_col << std::endl;
+    std::cout << "goal row:" << goal_row << "\ngoal col:" << goal_col << std::endl;
     return 42;
 }
 
 int astar_run(const std::vector<std::vector<int>>&grid, int start_row, int start_col, int goal_row, int goal_col) {
     std::cout << "Astar run called\n";
-    std::cout << "start row:" << " start_row" << "\nstart col:" << " start_col" << std::endl;
-    std::cout << "goal row:" << " goal_row" << "\ngoal col:" << " goal_col" << std::endl;
+    std::cout << "start row:" << start_row << "\nstart col:" << start_col << std::endl;
+    std::cout << "goal row:" << goal_row << "\ngoal col:" << goal_col << std::endl;
     return 43;
 }
 
 int dfs_run(const std::vector<std::vector<int>>&grid, int start_row, int start_col, int goal_row, int goal_col) {
     std::cout << "dfs run called\n";
-    std::cout << "start row:" << " start_row" << "\nstart col:" << " start_col" << std::endl;
-    std::cout << "goal row:" << " goal_row" << "\ngoal col:" << " goal_col" << std::endl;
+    std::cout << "start row:" << start_row << "\nstart col:" << start_col << std::endl;
+    std::cout << "goal row:" << goal_row << "\ngoal col:" << goal_col << std::endl;
     return 44;
 }
 
 int bfs_run(const std::vector<std::vector<int>>&grid, int start_row, int start_col, int goal_row, int goal_col) {
     std::cout << "bfs run called\n";
-    std::cout << "start row:" << " start_row" << "\nstart col:" << " start_col" << std::endl;
-    std::cout << "goal row:" << " goal_row" << "\ngoal col:" << " goal_col" << std::endl;
+    std::cout << "start row: " << start_row << "\nstart col: " << start_col << std::endl;
+    std::cout << "goal row:" << goal_row << "\ngoal col:" << goal_col << std::endl;
     return 45;
 }
